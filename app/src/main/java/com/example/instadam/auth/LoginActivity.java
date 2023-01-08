a26f19a8350d5a64512637c62277ff93d8ba52e0
Colin de Seroux <colindeseroux@gmail.com>
2023-01-08 11:40:29 +0100

    :sparkles: login/register xml page

--- app/src/main/java/com/example/instadam/auth/LoginActivity.java
+++ app/src/main/java/com/example/instadam/auth/LoginActivity.java
@@ -11,6 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
     }
 }
--- app/src/main/java/com/example/instadam/auth/RegisterActivity.java
+++ app/src/main/java/com/example/instadam/auth/RegisterActivity.java
@@ -11,6 +11,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        this.getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
     }
 }
