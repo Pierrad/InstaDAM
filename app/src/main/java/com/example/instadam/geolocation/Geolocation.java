d712fde48b1b3fd1714fa0d15850d57d138043a6
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2023-01-17 10:10:18 +0100

    🐛 Fix heavy Geocoder Call

--- app/src/main/java/com/example/instadam/geolocation/Geolocation.java
+++ app/src/main/java/com/example/instadam/geolocation/Geolocation.java
@@ -5,14 +5,13 @@ package com.example.instadam.geolocation;
  * It contains the latitude and longitude of the geolocation and the address calculated using Geocoder API.
  */
 public class Geolocation {
-    private double latitude;
-    private double longitude;
+    private final double latitude;
+    private final double longitude;
     private String address = "";
 
-    public Geolocation(double latitude, double longitude, String address) {
+    public Geolocation(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
-        this.address = address;
     }
 
     public double getLatitude() {
@@ -25,5 +24,9 @@ public class Geolocation {
 
     public String getAddress() {
         return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
